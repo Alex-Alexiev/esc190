5d9916b445f756e7079e7b8c3061eb75c13b4aa5
robert-purcaru <robert.purcaru@mail.utoronto.ca>
2021-02-22 16:53:21 -0500

    Project 1

---
+++ Projects/Project1/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+#include "autocomplete.h"
+
+
+
+int main(void)
+{
+    struct term *terms;
+    int nterms;
+    read_in_terms(&terms, &nterms, "cities.txt");
+    lowest_match(terms, nterms, "Mex");
+    highest_match(terms, nterms, "Mex");
+    
+    struct term *answer;
+    int n_answer;
+    autocomplete(&answer, &n_answer, terms, nterms, "Tor");
+    print_term_list(answer, n_answer);
+    //free allocated blocks here -- not required for the project, but good practice
+    return 0;
+}
+
+
+
